6bddb877d75e736a56568095d38e335298f5266a
yuhuidog@163.com <yuhuidog@163.com>
2020-06-30 17:08:01 +0800

    6/30LKT

--- miniprogram/pages/envelope/envelope.js
+++ miniprogram/pages/envelope/envelope.js
@@ -6,14 +6,14 @@ Page({
     images: {}
   },
   imageLoad: function (e) {
-    var $width = e.detail.width,    //获取图片真实宽度
+      var $width = e.detail.width,    //获取图片真实宽度
       $height = e.detail.height,
       ratio = $width / $height;    //图片的真实宽高比例
-    var viewWidth = 718,           //设置图片显示宽度，左右留有16rpx边距
+      var viewWidth = 718,           //设置图片显示宽度，左右留有16rpx边距
       viewHeight = 718 / ratio;    //计算的高度值
-    var image = this.data.images;
-    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
-    image[e.target.dataset.index] = {
+      var image = this.data.images;
+      //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+      image[e.target.dataset.index] = {
       width: viewWidth,
       height: viewHeight
     }
