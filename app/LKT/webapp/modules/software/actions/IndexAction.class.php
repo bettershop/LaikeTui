8342e4c2b8d88b28022ea67ed4526933d7d2f1eb
yuhuidog@163.com <yuhuidog@163.com>
2020-09-09 13:55:41 +0800

    优化：代码

--- app/LKT/webapp/modules/software/actions/IndexAction.class.php
+++ app/LKT/webapp/modules/software/actions/IndexAction.class.php
@@ -23,47 +23,26 @@ class IndexAction extends Action {
     public function getDefaultView() {
 
         $db = DBAction::getInstance();
-
         $request = $this->getContext()->getRequest();
-
         $sql = "select * from lkt_config where id = '1'";
-
         $r = $db->select($sql);
-
         $uploadImg = $r[0]->uploadImg; // 图片上传位置
-
         $upload_file = $r[0]->upload_file . '/'; // 文件上传位置
 
-
-
         // 查询插件表
-
         $sql = "select * from lkt_software";
-
         $r = $db->select($sql);
-
         $array1 = array('2' => '商业版', '1' => '运营版', '0' =>'开源版');
-
         $array2 = array('0' => '小程序', '1' => 'APP', '2' =>'后台');
-
         $software = [];
-
         foreach ($r as $key => $value) {
-
             foreach ($array2 as $k => $v) {
-
                 if($value->type == $k){
-
                     $value->type = $array2[$k];
-
                 }
-
                 if($value->software_version ==$k){
-
                     $value->software_version = $array1[$k];
-
                 }
-
                 $software[$key] = $value;
 
             }
@@ -71,11 +50,8 @@ class IndexAction extends Action {
         }
 
         $request->setAttribute("uploadImg",$uploadImg);
-
         $request->setAttribute("upload_file",$upload_file);
-
         $request->setAttribute("list",$software);
-
         return View :: INPUT;
 
     }
