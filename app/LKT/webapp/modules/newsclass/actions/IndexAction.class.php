894d867ea3186559a3b40dc5a888c8780c11197b
bettershop <yuhuidog@163.com>
2020-09-29 13:58:53 +0800

    优化

--- app/LKT/webapp/modules/newsclass/actions/IndexAction.class.php
+++ app/LKT/webapp/modules/newsclass/actions/IndexAction.class.php
@@ -1,12 +1,10 @@
 <?php
 
 /**
-
  * [Laike System] Copyright (c) 2017-2020 laiketui.com
-
  * Laike is not a free software, it under the license terms, visited http://www.laiketui.com/ for more details.
-
  */
+
 require_once(MO_LIB_DIR . '/DBAction.class.php');
 require_once(MO_LIB_DIR . '/ShowPager.class.php');
 require_once(MO_LIB_DIR . '/Tools.class.php');
--- app/LKT/webapp/modules/newsclass/actions/addAction.class.php
+++ app/LKT/webapp/modules/newsclass/actions/addAction.class.php
@@ -1,12 +1,9 @@
 <?php
-
 /**
-
  * [Laike System] Copyright (c) 2017-2020 laiketui.com
-
  * Laike is not a free software, it under the license terms, visited http://www.laiketui.com/ for more details.
-
  */
+
 require_once(MO_LIB_DIR . '/DBAction.class.php');
 
 class addAction extends Action {
