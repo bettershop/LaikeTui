1cacaaa2933bacc5cb16ea7506c17b44b3702d75
bettershop <yuhuidog@163.com>
2021-08-23 17:41:09 +0800

    优化

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -1,29 +1,20 @@
-// pages/about/about.js
 Page({
   data: {
   },
-
   onLoad: function (options) {
   },
-
   onReady: function () {
   },
-
   onShow: function () {
   },
-
   onHide: function () {
   },
-
   onUnload: function () {
   },
-
   onPullDownRefresh: function () {
   },
-
   onReachBottom: function () {
   },
-
   onShareAppMessage: function () {
   }
   
