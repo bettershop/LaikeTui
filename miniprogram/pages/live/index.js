6eb41d97605dd3d0d9a5a030ece54bee8341ef89
bettershop <yuhuidog@163.com>
2022-02-10 17:32:20 +0800

    优化

--- miniprogram/pages/live/index.js
+++ miniprogram/pages/live/index.js
@@ -27,21 +27,12 @@ Page({
   onUnload: function () {
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
   }
 })
